f69f123a89e48075de9eba7f99b7d4630e5dbc55
Jesse Rushlow <jr@geeshoe.com>
2020-03-15 09:58:12 -0400

    refactor test

--- tests/FunctionalTests/VerifyHelperFunctionalTest.php
+++ tests/FunctionalTests/VerifyHelperFunctionalTest.php
@@ -6,66 +6,70 @@ use PHPUnit\Framework\TestCase;
 use SymfonyCasts\Bundle\VerifyUser\Util\QueryUtility;
 use SymfonyCasts\Bundle\VerifyUser\Util\UriSigningWrapper;
 use SymfonyCasts\Bundle\VerifyUser\VerifyHelper;
+use SymfonyCasts\Bundle\VerifyUser\VerifyHelperInterface;
 
 class VerifyHelperFunctionalTest extends TestCase
 {
+    private const FAKE_SIGNING_KEY = 'superSecret';
+
+    /** @var VerifyHelperInterface */
+    private $helper;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        $this->helper = new VerifyHelper(
+            new UriSigningWrapper(self::FAKE_SIGNING_KEY),
+            new QueryUtility(),
+            3600
+        );
+    }
+
     public function testGenerateSignature(): void
     {
-        $signingWrapper = new UriSigningWrapper('superSecret');
-        $queryUtil = new QueryUtility();
-        $lifetime = 3600;
-        $helper = new VerifyHelper($signingWrapper, $queryUtil, $lifetime);
-
         $userId = '1234';
         $email = 'jr@rushlow.dev';
 
-        $result = $helper->generateSignature($userId, $email);
+        $result = $this->helper->generateSignature($userId, $email);
+
         $parsedUri = parse_url($result->getSignature());
         parse_str($parsedUri['query'], $queryParams);
 
-        $expectedQueryParams['id'] = $userId;
         $expectedQueryParams['email'] = $email;
         $expectedQueryParams['expires'] = $queryParams['expires'];
+        $expectedQueryParams['id'] = $userId;
+
         ksort($expectedQueryParams);
         $expectedQueryString = http_build_query($expectedQueryParams);
 
         $expectedUri = '/?'.$expectedQueryString;
-        $expectedHash = \base64_encode(\hash_hmac('sha256', $expectedUri, 'superSecret', true));
+        $expectedHash = \base64_encode(\hash_hmac('sha256', $expectedUri, self::FAKE_SIGNING_KEY, true));
 
-        $resultHash = $queryParams['signature'];
-
-        self::assertTrue(\hash_equals($expectedHash, $resultHash));
+        self::assertTrue(\hash_equals($expectedHash, $queryParams['signature']));
     }
 
     public function testValidSignature(): void
     {
-        $signingWrapper = new UriSigningWrapper('superSecret');
-        $queryUtil = new QueryUtility();
-        $lifetime = 3600;
-        $helper = new VerifyHelper($signingWrapper, $queryUtil, $lifetime);
-
         $userId = '1234';
         $email = 'jr@rushlow.dev';
 
-        $expiresAt = (new \DateTimeImmutable('+1 hours'))->getTimestamp();
-
-        $queryParams['id'] = $userId;
         $queryParams['email'] = $email;
-        $queryParams['expires'] = $expiresAt;
+        $queryParams['expires'] = (new \DateTimeImmutable('+1 hours'))->getTimestamp();
+        $queryParams['id'] = $userId;
 
-        ksort($queryParams);
         $queryString = http_build_query($queryParams);
-
         $uriToSign = '/?'.$queryString;
-        $signature = \base64_encode(\hash_hmac('sha256', $uriToSign, 'superSecret', true));
+
+        $signature = \base64_encode(\hash_hmac('sha256', $uriToSign, self::FAKE_SIGNING_KEY, true));
         $queryParams['signature'] = $signature;
-        unset($queryParams['id']);
-        unset($queryParams['email']);
 
-        ksort($queryParams);
-        $expectedQueryString = http_build_query($queryParams);
-        $expectedSignedUri = '/?'.$expectedQueryString;
+        unset($queryParams['id'], $queryParams['email']);
+        \ksort($queryParams);
 
-        self::assertTrue($helper->isValidSignature($expectedSignedUri, $userId, $email));
+        $expectedSignedUri = '/?'.\http_build_query($queryParams);
+
+        self::assertTrue($this->helper->isValidSignature($expectedSignedUri, $userId, $email));
     }
 }
