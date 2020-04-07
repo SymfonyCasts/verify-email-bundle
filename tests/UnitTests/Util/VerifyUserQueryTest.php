b9b87951d984275ebae446aaaeb339f4ec08c42c
Jesse Rushlow <jr@geeshoe.com>
2020-04-07 18:02:52 -0400

    refactored queryUtil unit tests

--- tests/UnitTests/Util/VerifyUserQueryTest.php
+++ tests/UnitTests/Util/VerifyUserQueryTest.php
@@ -9,12 +9,25 @@
 
 namespace SymfonyCasts\Bundle\VerifyUser\Tests\UnitTests\Util;
 
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use SymfonyCasts\Bundle\VerifyUser\Model\VerifyUserQueryParam;
+use SymfonyCasts\Bundle\VerifyUser\Model\VerifyUserUrlComponents;
 use SymfonyCasts\Bundle\VerifyUser\Util\VerifyUserQueryUtility;
+use SymfonyCasts\Bundle\VerifyUser\Util\VerifyUserUrlUtility;
 
 class VerifyUserQueryTest extends TestCase
 {
+    /**
+     * @var MockObject|VerifyUserUrlUtility
+     */
+    private $mockUrlUtility;
+
+    protected function setUp(): void
+    {
+        $this->mockUrlUtility = $this->createMock(VerifyUserUrlUtility::class);
+    }
+
     public function testRemovesParamsFromQueryString(): void
     {
         $params = ['a' => 'foo', 'b' => 'bar', 'c' => 'baz'];
@@ -30,34 +43,79 @@ class VerifyUserQueryTest extends TestCase
         $path = '/verify?';
         $uri = $path.http_build_query($params);
 
-        $queryUtility = new VerifyUserQueryUtility();
+        $components = new VerifyUserUrlComponents();
+        $components->setPath('/verify');
 
-        $result = $queryUtility->removeQueryParam($collection, $uri);
-        $expected = $path.http_build_query(['b' => 'bar']);
+        $this->mockUrlUtility
+            ->expects($this->once())
+            ->method('parseUrl')
+            ->with($uri)
+            ->willReturn($components)
+        ;
 
-        self::assertSame($expected, $result);
+        $components->setQuery('b=bar');
+
+        $this->mockUrlUtility
+            ->expects($this->once())
+            ->method('buildUrl')
+            ->with($components)
+        ;
+
+        $queryUtility = new VerifyUserQueryUtility($this->mockUrlUtility);
+        $queryUtility->removeQueryParam($collection, $uri);
     }
 
     public function testAddsQueryParamsToUri(): void
     {
-        $params = ['a' => 'foo', 'b' => 'bar', 'c' => 'baz'];
+        $url = '/verify?a=foo&c=baz';
+        $queryParam = [['a' => 'foo', 'c' => 'baz']];
 
-        $path = '/verify?';
-        $expected = $path.http_build_query($params);
+        $components = new VerifyUserUrlComponents();
+        $components->setPath('/verify');
+        $components->setQuery(http_build_query($queryParam));
 
-        $collection = [];
+        $this->mockUrlUtility
+            ->expects($this->once())
+            ->method('parseUrl')
+            ->with($url)
+            ->willReturn($components)
+        ;
 
-        foreach ($params as $key => $value) {
-            $collection[] = new VerifyUserQueryParam($key, $value);
-        }
+        $queryParam['b'] = 'bar';
 
-        $exists = $collection[1];
-        unset($collection[1]);
-        $uri = $path.http_build_query([$exists->getKey() => $exists->getValue()]);
+        $components->setQuery(ksort($queryParam));
 
-        $queryUtil = new VerifyUserQueryUtility();
-        $result = $queryUtil->addQueryParams($collection, $uri);
+        $this->mockUrlUtility
+            ->expects($this->once())
+            ->method('buildUrl')
+            ->with($components)
+        ;
 
-        self::assertSame($expected, $result);
+        $queryUtil = new VerifyUserQueryUtility($this->mockUrlUtility);
+        $queryUtil->addQueryParams([new VerifyUserQueryParam('b', 'bar')], $url);
+    }
+
+    public function testGetsExpiryTimeFromQueryString(): void
+    {
+        $uri = '/?a=x&expires=1234567890';
+
+        $components = new VerifyUserUrlComponents();
+        $components->setPath('/');
+        $components->setQuery('a=x&expires=1234567890');
+
+        $this->mockUrlUtility
+            ->expects($this->once())
+            ->method('parseUrl')
+            ->with($uri)
+            ->willReturn($components)
+        ;
+
+        $queryUtility = new VerifyUserQueryUtility($this->mockUrlUtility);
+        $result = $queryUtility->getExpiryTimeStamp($uri);
+
+        self::assertSame(
+            1234567890,
+            $result
+        );
     }
 }
