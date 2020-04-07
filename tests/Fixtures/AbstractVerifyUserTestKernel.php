678b3439c4feec4f3e67d80b603a2683382868ec
Jesse Rushlow <jr@geeshoe.com>
2020-04-07 10:35:23 -0400

    added service definition tests

---
+++ tests/Fixtures/AbstractVerifyUserTestKernel.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the SymfonyCasts BUNDLE_NAME_HERE package.
+ * Copyright (c) SymfonyCasts <https://symfonycasts.com/>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymfonyCasts\Bundle\VerifyUser\Tests\Fixtures;
+
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\FrameworkBundle\Routing\Loader\Configurator\RoutingConfigurator;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use SymfonyCasts\Bundle\VerifyUser\SymfonyCastsVerifyUserBundle;
+
+/**
+ * @author Jesse Rushlow <jr@rushlow.dev>
+ *
+ * @internal
+ */
+class AbstractVerifyUserTestKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    private $cacheDir;
+
+    private $logDir;
+
+    public function __construct()
+    {
+        parent::__construct('test', true);
+    }
+
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle(),
+            new SymfonyCastsVerifyUserBundle(),
+        ];
+    }
+
+    public function getCacheDir()
+    {
+        if (null === $this->cacheDir) {
+            return \sys_get_temp_dir().'/cache'.\spl_object_hash($this);
+        }
+
+        return $this->cacheDir;
+    }
+
+    public function getLogDir()
+    {
+        if (null === $this->logDir) {
+            return \sys_get_temp_dir().'/logs'.\spl_object_hash($this);
+        }
+
+        return $this->logDir;
+    }
+
+    protected function configureRoutes(RoutingConfigurator $routes)
+    {
+    }
+
+    public function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
+    {
+        $container->loadFromExtension('framework', [
+            'secret' => 'fool',
+            'router' => [
+                'utf8' => true,
+            ],
+        ]);
+    }
+}
---
+++ tests/IntegrationTests/VerifyUserServiceDefinitionTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the SymfonyCasts BUNDLE_NAME_HERE package.
+ * Copyright (c) SymfonyCasts <https://symfonycasts.com/>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymfonyCasts\Bundle\VerifyUser\Tests\IntegrationTests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use SymfonyCasts\Bundle\VerifyUser\Tests\Fixtures\AbstractVerifyUserTestKernel;
+
+/**
+ * @author Jesse Rushlow <jr@rushlow.dev>
+ */
+final class VerifyUserServiceDefinitionTest extends TestCase
+{
+    public function bundleServiceDefinitionDataProvider(): \Generator
+    {
+        $prefix = 'symfonycasts.verify_user.';
+
+        yield [$prefix.'query_utility'];
+        yield [$prefix.'signing_wrapper'];
+        yield [$prefix.'helper'];
+    }
+
+    /**
+     * @dataProvider bundleServiceDefinitionDataProvider
+     */
+    public function testBundleServiceDefinitions(string $definition): void
+    {
+        $pass = new DefinitionPublicCompilerPass();
+        $pass->definition = $definition;
+
+        $kernel = new VerifyUserDefinitionTestKernel();
+        $kernel->compilerPass = $pass;
+        $kernel->boot();
+
+        $container = $kernel->getContainer();
+        $container->get($definition);
+
+        $this->expectNotToPerformAssertions();
+    }
+}
+
+final class DefinitionPublicCompilerPass implements CompilerPassInterface
+{
+    public $definition;
+
+    public function process(ContainerBuilder $container)
+    {
+        $container->getDefinition($this->definition)
+            ->setPublic(true)
+        ;
+    }
+}
+
+final class VerifyUserDefinitionTestKernel extends AbstractVerifyUserTestKernel
+{
+    public $compilerPass;
+
+    protected function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass($this->compilerPass);
+    }
+}
