adcc386944b66607d392522172a08abc9c63210f
Jesse Rushlow <jr@geeshoe.com>
2020-04-20 15:38:11 -0400

    fixed unit tests

--- tests/UnitTests/VerifyUserHelperTest.php
+++ tests/UnitTests/VerifyUserHelperTest.php
@@ -10,100 +10,107 @@
 namespace SymfonyCasts\Bundle\VerifyUser\Tests\UnitTests;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\PhpUnit\ClockMock;
 use Symfony\Component\Routing\RouterInterface;
 use SymfonyCasts\Bundle\VerifyUser\Exception\ExpiredSignatureException;
 use SymfonyCasts\Bundle\VerifyUser\Generator\VerifyUserTokenGenerator;
+use SymfonyCasts\Bundle\VerifyUser\Tests\Fixtures\VerifyUserFixtureUser;
 use SymfonyCasts\Bundle\VerifyUser\Util\VerifyUserQueryUtility;
 use SymfonyCasts\Bundle\VerifyUser\Util\VerifyUserUriSigningWrapper;
 use SymfonyCasts\Bundle\VerifyUser\VerifyUserHelper;
 use SymfonyCasts\Bundle\VerifyUser\VerifyUserHelperInterface;
+use function Amp\Promise\wait;
 
 /**
  * @author Jesse Rushlow <jr@rushlow.dev>
+ *
+ * @group time-sensitive
  */
 final class VerifyUserHelperTest extends TestCase
 {
     private $mockRouter;
     private $mockSigner;
     private $mockQueryUtility;
+    private $tokenGenerator;
 
     protected function setUp(): void
     {
+        ClockMock::register(VerifyUserHelper::class);
+
         $this->mockRouter = $this->createMock(RouterInterface::class);
         $this->mockSigner = $this->createMock(VerifyUserUriSigningWrapper::class);
         $this->mockQueryUtility = $this->createMock(VerifyUserQueryUtility::class);
+        $this->tokenGenerator = $this->createMock(VerifyUserTokenGenerator::class);
     }
 
     public function testSignatureIsGenerated(): void
     {
-        $uriToBeSigned = '/verify?id=1234&email=jr@rushlow.dev&expires=';
-        $signature = '?signature=abc';
-        $signedUri = $uriToBeSigned.$signature;
+        $expires = time() + 3600;
+
+        $expectedSignature = '/verify?signature=1234';
+
+        $this->tokenGenerator
+            ->expects($this->once())
+            ->method('createToken')
+            ->with('1234', 'jr@rushlow.dev', $expires)
+            ->willReturn('hashedToken')
+        ;
 
         $this->mockRouter
             ->expects($this->once())
             ->method('generate')
-            ->with('app_verify_route', [])
+            ->with('app_verify_route', ['token' => 'hashedToken', 'expires' => $expires])
             ->willReturn('/verify')
-        ;
-
-        $this->mockQueryUtility
-            ->expects($this->once())
-            ->method('addQueryParams')
-            ->with(self::isType('array'), '/verify')
-            ->willReturn($uriToBeSigned)
         ;
 
         $this->mockSigner
             ->expects($this->once())
             ->method('signUri')
-            ->with($uriToBeSigned)
-            ->willReturn($signedUri)
-        ;
-
-        $this->mockQueryUtility
-            ->expects($this->once())
-            ->method('removeQueryParam')
-            ->with(self::isType('array'), $signedUri)
-            ->willReturn($signature)
+            ->with('/verify')
+            ->willReturn($expectedSignature)
         ;
 
         $helper = $this->getHelper();
         $components = $helper->generateSignature('app_verify_route', '1234', 'jr@rushlow.dev');
 
-        self::assertSame($signature, $components->getSignature());
+        self::assertSame($expectedSignature, $components->getSignature());
     }
 
     public function testIsValidSignature(): void
     {
-        $timestamp = (new \DateTimeImmutable('+1 minutes'))->getTimestamp();
-        $expires = 'expires='.$timestamp;
-
-        $signature = '/?'.$expires.'&signature=abc';
-        $uriToBeVerified = '/?'.$expires.'signature=abc&user=123&email=jr@rushlow.dev';
+        $user = new VerifyUserFixtureUser();
+        $expires = time() + 3600;
+        $signature = '/verify?signature=1234';
 
         $this->mockQueryUtility
             ->expects($this->once())
             ->method('getExpiryTimeStamp')
             ->with($signature)
-            ->willReturn($timestamp)
+            ->willReturn($expires)
+        ;
+
+        $this->tokenGenerator
+            ->expects($this->once())
+            ->method('createToken')
+            ->with($user->id, $user->email, $expires)
+            ->willReturn('someToken')
         ;
 
         $this->mockQueryUtility
             ->expects($this->once())
-            ->method('addQueryParams')
-            ->with(self::isType('array'), $signature)
-            ->willReturn($uriToBeVerified)
+            ->method('getTokenFromQuery')
+            ->with($signature)
+            ->willReturn('someToken')
         ;
 
         $this->mockSigner
             ->expects($this->once())
             ->method('isValid')
-            ->with($uriToBeVerified)
+            ->with($signature)
         ;
 
         $helper = $this->getHelper();
-        $helper->isValidSignature($signature, '1234', 'jr@rushlow.dev');
+        $helper->isValidSignature($signature, $user->id, $user->email);
     }
 
     public function testExceptionThrownWithExpiredSignature(): void
@@ -131,6 +138,6 @@ final class VerifyUserHelperTest extends TestCase
 
     private function getHelper(): VerifyUserHelperInterface
     {
-        return new VerifyUserHelper($this->mockRouter, $this->mockSigner, $this->mockQueryUtility, new VerifyUserTokenGenerator('foo'), 3600);
+        return new VerifyUserHelper($this->mockRouter, $this->mockSigner, $this->mockQueryUtility, $this->tokenGenerator, 3600);
     }
 }
