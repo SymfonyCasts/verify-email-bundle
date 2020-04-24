5dee564c71639fe80bb76c38191675f3d2157f27
Jesse Rushlow <jr@geeshoe.com>
2020-04-24 15:44:03 -0400

    update copyright header

--- tests/UnitTests/Util/VerifyUserQueryTest.php
+++ tests/UnitTests/Util/VerifyUserQueryTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the SymfonyCasts BUNDLE_NAME_HERE package.
+ * This file is part of the SymfonyCasts VerifyEmailBundle package.
  * Copyright (c) SymfonyCasts <https://symfonycasts.com/>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
