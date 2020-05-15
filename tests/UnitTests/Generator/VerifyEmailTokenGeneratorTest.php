2004f7ea74dc84824237082a522a41a11627284a
Jesse Rushlow <jr@geeshoe.com>
2020-05-15 15:25:39 -0400

    cs fixer it up

--- tests/UnitTests/Generator/VerifyEmailTokenGeneratorTest.php
+++ tests/UnitTests/Generator/VerifyEmailTokenGeneratorTest.php
@@ -1,9 +1,16 @@
 <?php
 
+/*
+ * This file is part of the SymfonyCasts VerifyEmailBundle package.
+ * Copyright (c) SymfonyCasts <https://symfonycasts.com/>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SymfonyCasts\Bundle\VerifyEmail\Tests\UnitTests\Generator;
 
-use SymfonyCasts\Bundle\VerifyEmail\Generator\VerifyEmailTokenGenerator;
 use PHPUnit\Framework\TestCase;
+use SymfonyCasts\Bundle\VerifyEmail\Generator\VerifyEmailTokenGenerator;
 
 /**
  * @author Jesse Rushlow <jr@rushlow.dev>
