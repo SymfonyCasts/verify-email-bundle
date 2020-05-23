5070b96a65019fd5906a6dcf132c7c35d69c5de9
Ryan Weaver <ryan@thatsquality.com>
2020-05-23 09:07:58 -0400

    adding interface docs

--- src/Exception/VerifyEmailExceptionInterface.php
+++ src/Exception/VerifyEmailExceptionInterface.php
@@ -10,10 +10,15 @@
 namespace SymfonyCasts\Bundle\VerifyEmail\Exception;
 
 /**
+ * An exception that is thrown by VerifyEmailHelperInterface::validateEmailConfirmation().
+ *
  * @author Jesse Rushlow <jr@rushlow.dev>
  * @author Ryan Weaver   <ryan@symfonycasts.com>
  */
 interface VerifyEmailExceptionInterface extends \Throwable
 {
+    /**
+     * Returns a safe string that describes why verification failed.
+     */
     public function getReason(): string;
 }
