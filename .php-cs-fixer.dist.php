fe6fd2068efbae3a7a54655144b25e5e0e1e1887
Jesse Rushlow <jr@rushlow.dev>
2021-05-04 10:06:14 -0400

    [ci] upgrade php-cs-fixer 2.x -> 3.0

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,18 +4,14 @@ if (!file_exists(__DIR__.'/src') || !file_exists(__DIR__.'/tests')) {
     exit(0);
 }
 
-$finder = PhpCsFixer\Finder::create()
+$finder = (new \PhpCsFixer\Finder())
     ->in([__DIR__.'/src', __DIR__.'/tests'])
 ;
 
-return PhpCsFixer\Config::create()
+return (new \PhpCsFixer\Config())
     ->setRules(array(
         '@Symfony' => true,
         '@Symfony:risky' => true,
-        '@PHPUnit75Migration:risky' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'protected_to_private' => false,
-        'semicolon_after_instruction' => false,
         'phpdoc_to_comment' => false,
         'header_comment' => [
             'header' => <<<EOF
