f04df433321b0ec92591d903b9f88037578fd513
Jesse Rushlow <40327885+jrushlow@users.noreply.github.com>
2024-04-04 06:58:01 -0400

    minor #175[ci] php-cs-fixer the php-cs-fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,15 +1,22 @@
 <?php
 
+/*
+ * This file is part of the SymfonyCasts VerifyEmailBundle package.
+ * Copyright (c) SymfonyCasts <https://symfonycasts.com/>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 if (!file_exists(__DIR__.'/src') || !file_exists(__DIR__.'/tests')) {
     exit(0);
 }
 
-$finder = (new \PhpCsFixer\Finder())
+$finder = (new PhpCsFixer\Finder())
     ->in([__DIR__.'/src', __DIR__.'/tests'])
 ;
 
-return (new \PhpCsFixer\Config())
-    ->setRules(array(
+return (new PhpCsFixer\Config())
+    ->setRules([
         '@Symfony' => true,
         '@Symfony:risky' => true,
         'phpdoc_to_comment' => false,
@@ -20,8 +27,8 @@ Copyright (c) SymfonyCasts <https://symfonycasts.com/>
 For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 EOF
-        ]
-    ))
+        ],
+    ])
     ->setRiskyAllowed(true)
     ->setFinder($finder)
 ;
