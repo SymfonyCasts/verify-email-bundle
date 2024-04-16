a68da87692fa875d461dc47323abfa68174c0a66
Jesse Rushlow <jr@rushlow.dev>
2024-04-16 19:00:12 -0400

    wip - use new bundle strategy

--- src/SymfonyCastsVerifyEmailBundle.php
+++ src/SymfonyCastsVerifyEmailBundle.php
@@ -9,22 +9,37 @@
 
 namespace SymfonyCasts\Bundle\VerifyEmail;
 
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-use SymfonyCasts\Bundle\VerifyEmail\DependencyInjection\SymfonyCastsVerifyEmailExtension;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 /**
  * @author Jesse Rushlow <jr@rushlow.dev>
  * @author Ryan Weaver   <ryan@symfonycasts.com>
  */
-class SymfonyCastsVerifyEmailBundle extends Bundle
+class SymfonyCastsVerifyEmailBundle extends AbstractBundle
 {
-    public function getContainerExtension(): ?ExtensionInterface
-    {
-        if (!$this->extension) {
-            $this->extension = new SymfonyCastsVerifyEmailExtension();
-        }
+    protected string $extensionAlias = 'symfonycasts_verify_email';
 
-        return $this->extension;
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+                ->integerNode('lifetime')
+                    ->defaultValue(3600)
+            ->info('The length of time in seconds that a signed URI is valid for after it is created.')
+            ->end()
+        ;
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/verify_email_services.xml');
+
+        $container->services()
+            ->get('symfonycasts.verify_email.helper')
+            ->arg(3, $config['lifetime'])
+        ;
     }
 }
